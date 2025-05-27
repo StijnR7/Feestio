21b899d65e41228e27d61f86841d6241f0c77b4e
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2025-05-27 13:21:49 +0200

    Seperated app and admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {getDocs, collection, doc} from 'firebase/firestore'
+import {getDocs, collection, doc, addDoc, deleteDoc, updateDoc} from 'firebase/firestore'
 import { db } from './config/firebase';
 import {useEffect, useState} from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Admin from "./admin/admin";
 function App() {
-    const [getTaskList, setTaskList] = useState([]);
-
-    useEffect(()=>{
-        const getTasks = async () => {
-            const data = await getDocs(collection(db, "tasks"));
-            setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-        }
-        getTasks();
-    },[])
-
+   
     return (
         <>
-            {getTaskList.map(task => {
-                return (
-                    <h2 key={task.id}>{task.title}</h2>
-                )
-            })}
+           <Routes>
+            <Route path="/" element={<Admin/>}/>
+            <Route path="party/:id" element={<Admin/>}/>
+           </Routes>
         </>
     )
 
---
+++ src/admin/admin.jsx
@@ -0,0 +1,82 @@
+
+import './admin.css'
+import {getDocs, collection, doc, addDoc, deleteDoc, updateDoc} from 'firebase/firestore'
+import { db } from '../config/firebase';
+import {useEffect, useState} from "react";
+
+function Admin() {
+    const [getPartyList, setPartyList] = useState([]);
+
+    const  DeleteItem = async (e) => {
+        e.preventDefault();
+        const itemID = e.target.value;
+        await deleteDoc(doc(db, "Party", itemID))
+
+
+    }
+    const UpdateItem = async (e) => {
+        e.preventDefault();
+        
+
+
+    }
+
+    const  AddItem = async (e) => {
+        e.preventDefault(); 
+        const docRef = await addDoc(collection(db, "Party"), {
+        Title: e.target.Title.value,
+        Location: e.target.Location.value,
+        StartTime: e.target.StartTime.value,
+        EndTime: e.target.EndTime.value,
+        OrganizationID: 1
+        });
+
+    }
+    useEffect(()=>{
+        const getParty = async () => {
+            const data = await getDocs(collection(db, "Party"));
+            setPartyList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        }
+        getParty();
+    },[])
+
+    return (
+        <>
+           
+         
+            <form onSubmit={AddItem}>
+                    <p>
+                        Title
+                    </p>
+                    <input placeholder='Party name' type="text" name="Title"  />
+                    <p>
+                        Location
+                    </p>
+                    <input name="Location" type="text" />
+                    <p>
+                        Start Time
+                    </p>
+                   
+                    <input name="StartTime" type="datetime-local" />
+                    <p>
+                        End Time
+                    </p>
+                    <input name="EndTime" type="datetime-local" />
+                   <br />
+                    <button type="submit">go</button>
+            </form>
+            {getPartyList.map(Party => {
+                
+                return (
+                    <>
+                    <h2 key={Party.id}>{Party.Title}</h2>
+                    <button onClick={DeleteItem} value={Party.id}>Delete</button>
+                    </>
+                )
+            })}
+        </>
+    )
+
+}
+
+export default Admin
