984cc7ae6e03838bc7d4a1779dbf2f89d30fa861
Stijn <1207024@student.roc-nijmegen.nl>
2025-06-18 00:38:45 +0200

    Added registering

--- src/partyDetails/partyDetails.jsx
+++ src/partyDetails/partyDetails.jsx
@@ -1,12 +1,45 @@
 import './partyDetails.css'
-import { useLocation } from 'react-router';
-
+import {useState, useEffect} from 'react';
+import { useLocation,} from 'react-router';
+import {
+  getDocs,
+  collection,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { auth, db } from "../config/firebase";
 function PartyDetails() {
   const location = useLocation();  
   const party = location.state;
+  const [user, setUser] = useState(null);
+
+ useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        fetchRequests(currentUser.uid);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const Register = async (e) => {
+    e.preventDefault();
+
+
+    await addDoc(collection(db, "registrations"), {
+      partyID: party.id,
+      uid: user.uid
+      
+    });
+  };
 
   return (
+
     <div className="party-container">
+      
       <h2>{party.Title}</h2>
       <p><strong>Location:</strong> {party.Location}</p>
       <p><strong>Start:</strong> {party.StartTime}</p>
@@ -14,6 +47,7 @@ function PartyDetails() {
       {party.ImageURL && (
         <img src={party.ImageURL} alt={party.Title} />
       )}
+      <button onClick={Register}>Register</button>
     </div>
   );
 }
